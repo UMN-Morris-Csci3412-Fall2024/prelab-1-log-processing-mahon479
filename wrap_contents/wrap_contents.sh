7dd6cdf8890ac18bbc45de420450189a755f3ab4
mahon479 <mahon479@morris.umn.edu>
2024-09-05 10:27:31 -0500

    Created the script for wrap_contents.sh, all tests are passing.

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script takes in three arguments: the contents to wrap, the header specifier, and the output file name
+contents=$1
+header_specifier=$2
+result=$3
+
+# Create the header and footer files names
+header_file="${header_specifier}_header.html"
+footer_file="${header_specifier}_footer.html"
+
+# Joining the contents, header, and footer into the result file
+cat $header_file $contents $footer_file > $result
+
